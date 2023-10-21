930b61fd4955b8a811ca4a9c6ceb1f5b723cfc20
aayardev <aayarismail2000@gmail.com>
2023-10-21 18:54:35 +0100

    feat: handle errors

--- quizify-front/src/app/error.tsx
+++ quizify-front/src/app/error.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import QuizifyLogo from "@/components/QuizifyLogo";
+import { Button } from "@/components/ui/button";
+import ButtonLink from "@/components/ui/button-link";
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
+
 export default function Error({
   error,
   reset,
@@ -8,9 +14,20 @@ export default function Error({
   reset: () => void;
 }) {
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button onClick={() => reset()}>Try again</button>
-    </div>
+    <>
+      <QuizifyLogo className="fixed top-5 sm:z-10   left-8 " />
+      <div className="absolute inset-0 h-scree w-screen flex items-center justify-center">
+        <div>
+          <h2 className="text-3xl font-medium">Something went wrong!</h2>
+
+          <div className="flex justify-center gap-x-4 mt-5">
+            <Button onClick={() => reset()}>Try again</Button>
+            <ButtonLink href="/" className="font-medium" variant="outline">
+              <MoveLeft className="inline-block mr-1.5" /> Go home
+            </ButtonLink>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
