18af89bfe16c261e9373894c12a0fefae22b2610
AAYAR Ismail <65457335+aayardev@users.noreply.github.com>
2023-11-08 17:03:46 +0100

    chore: Update build_files.sh

--- quizify-back/quizify/build_files.sh
+++ quizify-back/quizify/build_files.sh
@@ -1,5 +1,5 @@
 python3.9 -m venv env
 source ./env/bin/activate
-pip install -r ../requirements.txt
-python manage.py migrate
-python manage.py collectstatic
+./env/bin/pip install -r ../requirements.txt
+./env/bin/python manage.py migrate
+./env/bin/python manage.py collectstatic
